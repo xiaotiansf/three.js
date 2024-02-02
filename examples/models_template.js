3d971e0bf7ad940ec4bb7c1c9494b70e588dd5e1
xiaotiansf <xiaotianshimail@gmail.com>
2024-02-01 16:19:37 -0800

    add websocket client for receiving display commands.

--- examples/models_template.js
+++ examples/models_template.js
@@ -14,6 +14,13 @@ import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { IFCLoader } from 'web-ifc-three';
 import { IFCSPACE } from 'web-ifc';
 
+const timeout = 5000;
+let retrying = true;
+const socket = new WebSocket('ws://127.0.0.1:8080');
+// Connect
+console.log('Videoplayer: Connecting to ws://127.0.0.1:8080...');
+MakeConnection();
+
 let mixer, camera, scene, renderer, stats, loader, controls;
 let video, videotexture;
 
@@ -467,3 +474,56 @@ function animate() {
   stats.update();
   renderer.render( scene, camera );
 }
+
+// Functions to handle socket events
+function MakeConnection ()
+{
+    // Connection opened
+    socket.addEventListener('open', function (event) {
+        socket.send('Models: Hello Server!');
+        console.log('Models: connected to palacio-display-server!');
+        console.log("Models: current href: %s", window.location.href);
+        retrying = false;
+    });
+    if (socket.readyState !== socket.OPEN) {
+        console.log('Models: Connection open: failed and retry later');
+        setTimeout(MakeConnection, timeout);
+    }
+}
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+  console.log(event.data.toString());
+  let cmd_string = event.data.toString();
+  let hashtag_index = cmd_string.indexOf('{');
+  if (hashtag_index === 0) {
+      let json = cmd_string.slice(hashtag_index);
+      const obj = JSON.parse(json);
+      if (obj.cmd === 'video' ) {
+      }
+      if (obj.cmd === 'image' || obj.cmd === 'gif') {
+      }
+      else if (obj.cmd === 'model') {
+          let index = obj.filename.indexOf("image-uploads");
+          let zipfilename = obj.filename.substr(index);
+      }
+  }
+})
+
+socket.addEventListener('close', function (event) {
+  console.log('Connection closed');
+  if (!retrying) {
+      retrying = true;
+      console.log('Reconnecting...');
+  }
+  setTimeout(MakeConnection, timeout);
+})
+
+socket.addEventListener('error', function (event) {
+  console.log('Connection error: failed and retry later');
+  if (!retrying) {
+      retrying = true;
+      console.log('Reconnecting...');
+  }
+  setTimeout(MakeConnection, timeout);
+})
